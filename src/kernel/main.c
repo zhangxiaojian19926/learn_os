a12fbc33b43209db9b49c0e2787213e9927fef6d
zhangxiaojian <505314607@qq.com>
2023-12-17 11:55:34 +0800

    20231217 上传控制中断的方法+测试中断控制指令是否生效

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,4 +1,5 @@
 #include <onix/debug.h>
+#include <onix/types.h>
 
 #define LOGK(fmt, args...) DEBUGK(fmt, ##args)
 
@@ -12,7 +13,21 @@ extern void rtc_init();
 extern void memory_test();
 extern void memory_map_init();
 extern void mapping_init();
+
 extern void bitmap_test_1();
+extern bool interrupt_disable();
+extern bool get_interrupt_state();
+extern bool set_interrupt_state(bool state);
+
+// 交替开启或关闭中断
+void intr_test()
+{
+    bool intr = interrupt_disable();
+
+    // 设置堆栈
+    set_interrupt_state(intr);
+
+}
 
 void kernel_init()
 {
@@ -25,9 +40,16 @@ void kernel_init()
     // time_init();
     // rtc_init();
 
-    memory_test();
+    bool intr = interrupt_disable();
+    set_interrupt_state(true);
+    LOGK("start>>> intr:%d, getintr:%d\n", intr, get_interrupt_state());
 
-    asm volatile("sti");
+    intr = interrupt_disable();
+    LOGK("stop>>>> intr:%d, getintr:%d\n", intr, get_interrupt_state());
+
+    set_interrupt_state(true);
+    LOGK("start>>> lastintr:%d, getintr:%d\n", intr, get_interrupt_state());
+
     hang();
     
     // task_init();
