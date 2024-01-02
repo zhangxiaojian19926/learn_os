6ebc479472de641ec84293e95a992b739d3a9807
zhangxiaojian <505314607@qq.com>
2024-01-02 20:35:57 +0800

    20240102 修正自旋锁概念，改成锁的实现

--- src/kernel/mutex.c
+++ src/kernel/mutex.c
@@ -64,7 +64,7 @@ void mutex_unlock(mutex_t *mutex)
 }
 
 // 自旋锁初始化
-void spin_init(spinlock_t *lock)
+void lock_init(lock_t *lock)
 {
     lock->holder = NULL;
     lock->repeat = 0;
@@ -72,7 +72,7 @@ void spin_init(spinlock_t *lock)
 }
 
 // 尝试持有锁
-void spin_lock(spinlock_t *lock)
+void lock_acquire(lock_t *lock)
 {
     task_t *current = running_task();
 
@@ -91,7 +91,7 @@ void spin_lock(spinlock_t *lock)
 }
 
 // 解锁
-void spin_unlock(spinlock_t *lock)
+void lock_release(lock_t *lock)
 {
     task_t *current = running_task();
     assert(lock->holder == current);
--- src/kernel/thread.c
+++ src/kernel/thread.c
@@ -22,20 +22,20 @@ void idle_thread()
     }
 }
 
-spinlock_t lock;
+lock_t lock;
 
 // 1号线程
 void init_thread()
 {
-    spin_init(&lock);
+    lock_init(&lock);
     set_interrupt_state(true);
     u32 counter = 0;
 
     while (true)
     {
-        spin_lock(&lock);
+        lock_acquire(&lock);
         LOGK("init task... %d\n", counter++);
-        spin_unlock(&lock);
+        lock_release(&lock);
     }
 }
 
@@ -46,8 +46,8 @@ void test_thread()
 
     while (true)
     {
-        spin_lock(&lock);
+        lock_acquire(&lock);
         LOGK("test task... %d\n", counter++);
-        spin_unlock(&lock);
+        lock_release(&lock);
     }  
 }
